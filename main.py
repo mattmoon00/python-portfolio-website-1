70eb8c429e3bfb62868b45c320e83e2124ebcc57
Matt Moon <99843451+mattmoon00@users.noreply.github.com>
2023-04-12 15:32:57 -0600

    added photo, as well as small info about me

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-#portfolio website in python
+import streamlit as st
 
-print("Hello world")
+# st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Matt Moon")
+    content = """
+    Hi, I'm Matt. I am a 23 year old Account Executive/Programmer. 
+    I have experience in web development, python, 
+    and a tiny bit of swift.
+    """
+    st.write(content)
