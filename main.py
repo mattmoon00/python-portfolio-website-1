b568308cc4c65f4cc8a3db0712ad6167ad3c1411
Matt Moon <99843451+mattmoon00@users.noreply.github.com>
2023-04-12 15:35:36 -0600

    Updated the about me section

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ with col2:
     content = """
     Hi, I'm Matt. I am a 23 year old Account Executive/Programmer. 
     I have experience in web development, python, 
-    and a tiny bit of swift.
+    and a tiny bit of swift. I'm currently attending UVU, studying computer science.
+    I also attended Devmountain in 2022 and graduated in their web development program. 
+    I gained experience in HTML/CSS, react, javascript, SQL and node.js. 
+    I also have sales experience, working in tech sales for a company here in Utah.
+    In my free time I love to play tennis and golf, and spend time with my wife and daughter.
     """
     st.write(content)
